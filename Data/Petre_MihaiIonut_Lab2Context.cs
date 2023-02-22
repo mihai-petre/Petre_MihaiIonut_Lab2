5855a8b8fec481f187b25dcd521669738976222b
Petre Mihai-Ionut <petremihaiionut@gmail.com>
2023-02-22 19:56:41 +0200

    First commit - Laborator 2

---
+++ Data/Petre_MihaiIonut_Lab2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Petre_MihaiIonut_Lab2.Models;
+
+namespace Petre_MihaiIonut_Lab2.Data
+{
+    public class Petre_MihaiIonut_Lab2Context : DbContext
+    {
+        public Petre_MihaiIonut_Lab2Context (DbContextOptions<Petre_MihaiIonut_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Petre_MihaiIonut_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Petre_MihaiIonut_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Petre_MihaiIonut_Lab2.Models.Category> Category { get; set; }
+       
+    }
+}
